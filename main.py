469d42003210fbc49ceecddcf5f7d5ade62964e6
Malicarus <tylerarcher96@gmail.com>
2026-02-10 23:12:58 -0500

    added initial asteroid destruction

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ def main():
                 log_event("player_hit")
                 print("Game Over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid) == True:
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
