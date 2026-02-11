d7882c7c7095dc6e89a2f133bd608d5d1da86203
Malicarus <tylerarcher96@gmail.com>
2026-02-10 20:39:55 -0500

    added triangle player to screen

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
-    fps_clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    p = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
+        p.draw(screen)
         pygame.display.flip()
-        dt = fps_clock.tick(60) / 1000
-
+        
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
